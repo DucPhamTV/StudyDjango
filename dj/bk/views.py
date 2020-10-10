0c853dfa4eb19da0312149e6408f8dd4038069d4
DucPham <41200850@hcmut.edu.vn>
2020-10-10 21:18:06 +0700

    Pure django template website can work now

--- dj/bk/views.py
+++ dj/bk/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import ListView, View
 
 # Create your views here.
 from .models import Item
 
 # Create your views here.
-class IndexView(View):
+class IndexView(ListView):
     template_name = 'bk/index.html'
+    context_object_name = "items"
+
+    def get_queryset(self):
+        return Item.objects.all()
 
 
-class ItemView(View):
-    def get(self, request, name):
-        # return all question in database
-        item = Item.objects.get(name=name)
-        return render(request, 'bk/item.html', {
-            'image_url': item.image,
-            'description': item.paragraph,
-            'name': item.name,
-            #'date': item.date,
-        })
+#class ItemView(View):
+#    def get(self, request, name):
+#        # return all question in database
+#        item = Item.objects.get(name=name)
+#        return render(request, 'bk/item.html', {
+#            'image_url': item.image,
+#            'description': item.paragraph,
+#            'name': item.name,
+#            #'date': item.date,
+#        })
