a4299963e39f8be6f8f6f7b272b6cee12212f92e
DucPhamTV <phamcongduc1994@gmail.com>
2020-10-13 16:08:19 +0000

    Customize model to add more fields to Item

--- dj/bk/models.py
+++ dj/bk/models.py
@@ -10,10 +10,19 @@ def image_path(obj, filename):
 # Create your models here.
 
 class Item(models.Model):
+    DIFFICULTS = [
+        ("EASY", "Easy"),
+        ("MEDIUM", "Medium"),
+        ("HARD", "Hard"),
+        ("HARDEST", "Hardest"),
+    ]
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    paragraph = models.CharField(max_length=5000)
+    title = models.TextField(max_length=200, default="")
+    paragraph = models.TextField()
     image = models.FileField(upload_to=image_path)
+    difficult = models.CharField(max_length=10, choices=DIFFICULTS,
+                                 default="EASY")
 
     class Meta:
         ordering = ['name']
