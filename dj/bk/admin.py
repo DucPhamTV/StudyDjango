dda903634266a51ccacce0b18b348a6adfd81de9
DucPhamTV <phamcongduc1994@gmail.com>
2020-10-17 08:50:57 +0700

    Allow to add Images to Item

--- dj/bk/admin.py
+++ dj/bk/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Item, Category, Image, Comment
 
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 1
 
-admin.site.register(Item)
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Comment)
